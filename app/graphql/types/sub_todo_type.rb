5fa31d7de4dec92797e7e677621a244faba82396
ahmadsyahirmuhamad <ahmadsyahirmuhamad@gmail.com>
2017-11-05 02:27:20 +0800

    add todo_id

--- app/graphql/types/sub_todo_type.rb
+++ app/graphql/types/sub_todo_type.rb
@@ -2,6 +2,7 @@ Types::SubTodoType = GraphQL::ObjectType.define do
   name "SubTodo"
 
   field :id, !types.ID
+  field :todo_id, types.ID
   field :title, types.String
   field :completed, types.Boolean
   field :created_at, !types.String
